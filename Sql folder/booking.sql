4aff5fe5db24a137b8f661192c9807cb29c5c303
DESKTOP-8APM7MG\mdk19 <mdk1997@naver.com>
2024-04-11 14:37:28 +0900

    #7 sql폴더 생성및 데이터 추가

---
+++ Sql folder/booking.sql
@@ -0,0 +1,9 @@
+drop table if exists booking;
+create table booking(
+book_code int primary key NOT NULL auto_increment,
+book_date date NOT NULL,
+mem_code varchar(255) NOT NULL,
+medi_code varchar(255) NOT NULL,
+medi_count int NOT NULL,
+book_status CHAR(1) NOT NULL
+)ENGINE=INNODB;
